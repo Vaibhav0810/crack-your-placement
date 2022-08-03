c08fc27cfccb2dbe45f58d5c73b0be127ff7eade
Vaibhav Nirmal <73460717+Vaibhav0810@users.noreply.github.com>
2022-08-03 12:31:42 +0530

    Create Q122 Sum of subarray minimums.cpp

---
+++ day 25/Q122 Sum of subarray minimums.cpp
@@ -0,0 +1,23 @@
+Brute 
+Time-O(N^3)
+  Space- O(n)
+  
+  class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        
+        vector<int> ans;
+        int sum=0;
+        for(int i=0;i<arr.size();i++){
+            for(int j=i;j<arr.size();j++){
+                for(int k=i;k<=j;k++){
+                    ans.push_back(arr[k]);
+                }
+                sum=sum+*min_element(ans.begin(),ans.end());
+                
+                ans.clear();
+            }
+        }
+        return sum%1000000007;
+    }
+};
